bc36a1019ba63953e8fdaec80cb634601012b797
Halil Ibrahim <hibrahimbinol@gmail.com>
2023-01-13 23:36:47 +0300

    add Currency component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Currency from "./ui/Currency";
+
 function App() {
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center flex-col">
+      <Currency amount={1234.56} />
       <h1
         className="underline decoration-blue-500 font-bold text-center text-9xl"
       >Resurgence</h1>
---
+++ src/ui/Currency.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactComponent as Logo } from './gold-coin.svg'
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+type CurrencyProps = {
+  amount: number
+}
+
+const Currency = (props: CurrencyProps) => {
+  const amount = formatter.format(props.amount);
+  return <div className="
+  p-1 bg-gray-800
+  flex items-center
+  border border-2 border-gray-900 rounded-md divide-x divide-gray-900
+  ">
+    <Logo className="h-5 w-5 mr-1" />
+    <span className="pl-2">{amount}</span>
+  </div>;
+};
+
+export default Currency;
