bb9b9daf9643ae770773f48ef7879923f9ee72ef
Halil İbrahim Binol <hibrahimbinol@gmail.com>
2023-09-14 20:28:57 +0300

    currency component. svg plugin. export to all network.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Currency from "./ui/Currency.tsx";
 
 function App() {
 
   return (
-    <p className="text-3xl font-bold underline">Hello world!</p>
+    <div className="h-screen flex items-center justify-center flex-col">
+      <Currency amount={1234.56} />
+      <h1
+        className="underline decoration-blue-500 font-bold text-center text-9xl"
+      >Resurgence</h1>
+    </div>
   )
 }
 
