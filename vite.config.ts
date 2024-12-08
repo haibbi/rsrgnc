228de505e2d9e2c979c5d6d1534f63a17cae94cd
Halil İbrahim Binol <hibrahimbinol@gmail.com>
2024-12-08 21:20:33 +0300

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Resurgence",
+        short_name: "RSRGNC",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#d1d5db",
+        theme_color: "#374151",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 })
