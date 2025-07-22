238c429ef96baa3cde11203249dcb41c5cecfd74
Halil İbrahim Binol <hibrahimbinol@gmail.com>
2025-07-22 20:24:32 +0300

    tailwind and other version upgrade.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
